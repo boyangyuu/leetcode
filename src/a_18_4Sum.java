c524cc4f28167f6f1d9b55d59f6dab7dc18f06f9
ybyGold <415098820@qq.com>
2016-08-12 15:39:24 +0800

    init

---
+++ src/a_18_4Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class a_18_4Sum {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
