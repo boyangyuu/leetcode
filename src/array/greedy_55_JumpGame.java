4fd88bad81e4b6db992ff0724c2a4f1f5f1dad0c
ybyGold <415098820@qq.com>
2016-08-15 00:00:35 +0800

    #medium greedy_55_JumpGame create

---
+++ src/array/greedy_55_JumpGame.java
@@ -0,0 +1,50 @@
+package array;
+
+public class greedy_55_JumpGame {
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0};
+        //System.out.println(a_18_4Sum.fourSum(nums, 0));
+    }
+
+    public static boolean canJump(int[] nums) {
+        boolean ret = false;
+
+        return ret;
+    }
+
+}
+/** 题
+ *
+ * http:https://leetcode.com/problems/jump-game/
+ *
+ Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ Each element in the array represents your maximum jump length at that position.
+
+ Determine if you are able to reach the last index.
+
+ For example:
+ A = [2,3,1,1,4], return true.
+
+ A = [3,2,1,0,4], return false.
+
+ */
+
+/** Solution
+ * 时间  空间
+ *
+ *
+ *
+ 参考网站
+
+ 思路 :
+
+
+ 步骤1
+ 步骤2
+ 步骤3
+
+ bug1
+ bug2
+ bug3
+ */
