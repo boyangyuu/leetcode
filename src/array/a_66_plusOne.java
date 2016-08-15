74c25e612ac4e38f129d664ac3c41c0fc9b19417
ybyGold <415098820@qq.com>
2016-08-15 23:59:40 +0800

    #create  a_66_plusOne

---
+++ src/array/a_66_plusOne.java
@@ -0,0 +1,46 @@
+package array;
+
+public class a_66_plusOne {
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0};
+        //System.out.println(a_18_4Sum.fourSum(nums, 0));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        int [] ret = new int [digits.length];
+
+
+        return ret;
+    }
+}
+
+/** 题
+ *
+ * http:https://leetcode.com/problems/plus-one/
+ *
+ Given a non-negative number represented as an array of digits, plus one to the number.
+
+ The digits are stored such that the most significant digit is at the head of the list.
+
+给定 代表数字的 数组, 加1, 求结果(数组表示,需考虑进位)
+
+ */
+
+/** Solution
+ * 时间  空间
+ *
+ *
+ *
+ 参考网站
+
+ 思路 :
+
+
+ 步骤1
+ 步骤2
+ 步骤3
+
+ bug1
+ bug2
+ bug3
+ */
