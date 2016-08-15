c0cc1ffe9bf9a1467e1872ec5941d964e8257b6a
ybyGold <415098820@qq.com>
2016-08-15 23:45:23 +0800

    #bug time a_45_jumpGame2

--- src/array/a_45_jumpGame2.java
+++ src/array/a_45_jumpGame2.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.ArrayList;
+
 public class a_45_jumpGame2 {
     public static void main(String[] args) {
         int[] nums = {0,0,0,0};
@@ -7,6 +9,34 @@ public class a_45_jumpGame2 {
     }
 
     public static int jump(int[] nums) {
+
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return 0;
+        int l = nums.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        for (int i = 1; i < nums.length; i++) {
+            //limit
+            if (list.size() == 1) {
+                list.add(i);
+                continue;
+            }
+
+            // check to delete object in list
+            int check = list.get(list.size() - 2);
+            if (nums[l - i] < i - check) { //bug
+                list.add(i);
+            } else {
+                list.remove(list.size() - 1);
+                i--; // bug1
+            }
+
+        }
+        return list.size() - 1;
+    }
+
+
+    public static int jump1(int[] nums) {
 
         if (nums.length == 0) return 0;
         if (nums.length == 1) return 0;
@@ -63,6 +93,18 @@ gg exceed Time
 
 
  方法2 入站
+ 7,5,2,3,1,1,4
+ {index}
+
+ nums[l - index] < index(该元素位置) - 倒数第二元素(上上一个jump点位置) 则入战 否则递归删除,  当 栈大小为1 入栈
+ {0, 1} 倒数第二个作为起点 开始
+ {0, 1} => {0, 1, 2}  倒数i = 2 尝试替换 末尾元素, 与 末尾之前元素 进行比较 nums[l - 2] < 2 - 0 替换失败 入站
+ {0,1,2} => {0,3}     nums[l - 3] >  3 - 1 删, {0,1}  nums[l-3] >= 3-0 删, 大小为1 入
+ .....
+ index = 0 退出
+
+
+
 
 
 
