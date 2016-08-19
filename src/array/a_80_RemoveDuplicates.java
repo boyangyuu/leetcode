ce124dbdb987272d63a9c65fa2383b767fd3b1e7
ybyGold <415098820@qq.com>
2016-08-20 00:25:31 +0800

    #accepted a_80_RemoveDuplicates

--- src/array/a_80_RemoveDuplicates.java
+++ src/array/a_80_RemoveDuplicates.java
@@ -2,27 +2,25 @@ package array;
 
 public class a_80_RemoveDuplicates {
     public static void main(String[] args) {
-        int[] nums = {0,0,0,0};
-        //System.out.println(a_18_4Sum.fourSum(nums, 0));
+        int[] nums = {0,0,0};
+        System.out.println(a_80_RemoveDuplicates.removeDuplicates(nums));
     }
 
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
         if (nums.length < 3) return nums.length;
-        int i = 0, j = 0;
-        int c = 0;
+        int i = 0, j = 1;
+
         while(j < nums.length){
-            if (nums[i] != nums[j]) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
                 nums[i] = nums[j];
-                c = 1;
                 i++;
                 j++;
             } else {
-                if (c <= 2) {
-                    nums[i] = nums[j];
+                if (nums[i] != nums[j]) {
+                    nums[i + 1] = nums[j];
                     i++;
                     j++;
-                    c++;
                 } else {
                     j++;
                 }
@@ -35,20 +33,45 @@ public class a_80_RemoveDuplicates {
 }
 /** 题
  *
- * http:
+ * http:https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
  *
 
+ Follow up for "Remove Duplicates":
+ What if duplicates are allowed at most twice?
 
+ For example,
+ Given sorted array nums = [1,1,1,2,2,3],
+
+ Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
  */
 
 /** Solution
  * 时间  空间
  *
  *
- *
+ *k
  参考网站
 
  思路 :
+#######本道题 debug很多次 建议熟悉思路 不要用cnt方法数数!
+
+两个指针, i = 0, j = 1, j 试图去复制到 i的下一个位置
+ 当 i == 0 直接复制
+当 [i] [i-1] 相同时, 则需要判断[j] , [i] == [j] 则 j++ 否则 可以复制到 i+1 且 i++ j++
+ 当[i] [i-1] 不同时, 则可以复制
+
+ 延伸可以单独写方法去判断 i-1,i-2,i ....
+
+
+ 开始 1 2 2 2 3 i = 0, j = 1,  i == 0  复制
+ 1 2 2 2 3 i = 1, j = 2, 复制
+ 1 2 2 2 3 i = 2, j = 3, 2 == 2 无法复制
+ 1 2 2 3 3 i = 2, j = 4, 2 != 3 复制
+
+1 1 1 i = 0 j = 0 c = 1
+ 1 1 1 i = 1 j = 1 c = 2
+ 1 1 1 i = 1 j = 2 c = 2
+
 
 
  步骤1
