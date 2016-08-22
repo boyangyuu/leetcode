f6a78e4d1d98b404f4d2598867ba53ffdc3d49fe
ybyGold <415098820@qq.com>
2016-08-22 21:10:38 +0800

    create bug a_54_SpiralMatrix

--- src/array/a_54_SpiralMatrix.java
+++ src/array/a_54_SpiralMatrix.java
@@ -9,49 +9,28 @@ public class a_54_SpiralMatrix {
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ret = new ArrayList<Integer>();
+        List<Integer> ret = new ArrayList();
+        if(matrix.length == 0 || matrix[0].length == 0) return ret;
+        int m = matrix.length, n = matrix[0].length;
+        int r = Math.min(m,n) + 1 / 2;
 
-        int il = 0, jl = matrix.length - 1, r = 0;
-        int i = 0, j = 0, l = matrix.length;
+        //循环圈数
+        for (int i = 0; i < r; i++, m-=2, n-=2) {
+            for (int j = i; j < i+n; j++) //bug1 里面要有i
+                ret.add(matrix[i][j]);  //固定 横坐标 i
 
-        int end = l % 2 == 0 ? l / 2 - 1 : (int)Math.floor(l / 2);
-        int c = 10;
-        while(c-- != 0){
-            //out
-            System.out.println("ret:" + ret);
-            if (i == end && j == end) break;
+            for (int j = i + 1; j < i+m; j++)
+                ret.add(matrix[j][i+m-1]); //固定纵坐标 i + m - 1
 
-            //add
-
-            if (i == il){
-                if (j < jl) {
-                    ret.add(matrix[i][j]);
-                    j++;
-                    System.out.println("add j: " + j);
-                }
-                else {
-                    il = l - r - 1 - il; // 3 - 0 - 1 = 2
-
-                }
-            }
-            else if (j == jl) {
-                if (i < il) {
-                    ret.add(matrix[i][j]);
-
-                    i++;
-                    System.out.println("add i: " + i);
-                }
-                else {
-                    jl = r;
-                }
+            if (m == 1) break;
+            for (int j = i+n-2; j >= i; j--) {
+                ret.add(matrix[i+n-1][j]);   //固定横坐标 bug2
             }
 
-            //next round
-            if (i == j && i < end){
-                r++;
-                i++;
-                j++;
+            for (int j = i+m-2; j >= i + 1; j--) {
+                ret.add(matrix[j][i]);
             }
+
         }
 
 
@@ -86,9 +65,11 @@ public class a_54_SpiralMatrix {
  https://discuss.leetcode.com/topic/3713/super-simple-and-easy-to-understand-solution/21
  思路 :
 
- 错误: 创造边界之后搞, 
- 正确 1,2,3,4 标志位  之后走位
- 
+ 错误: 创造边界之后搞,
+
+ 确定圈数 min(m,n) + 1 / 2
+
+
 
 
  步骤1
@@ -96,6 +77,9 @@ public class a_54_SpiralMatrix {
  步骤3
 
  bug1
+ 确定边界
+ 注意 参考网站的颜色区别...每次划线到头
+
  bug2
  bug3
  */
