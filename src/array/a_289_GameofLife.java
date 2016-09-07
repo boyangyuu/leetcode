139f7df4e39a2f1d0144a7e07acaa5ef4280d993
ybyGold <415098820@qq.com>
2016-09-07 21:36:00 +0800

    create bug  a_289_GameofLife

---
+++ src/array/a_289_GameofLife.java
@@ -0,0 +1,81 @@
+package array;
+
+public class a_289_GameofLife {
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0};
+        //System.out.println(a_18_4Sum.fourSum(nums, 0));
+    }
+
+    /*
+1. 如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡
+
+2. 如果活细胞周围八个位置有两个或三个活细胞，则该位置活细胞仍然存活
+
+3. 如果活细胞周围八个位置有超过三个活细胞，则该位置活细胞死亡
+
+4. 如果死细胞周围正好有三个活细胞，则该位置死细胞复活
+
+    0 0-0
+    1 1-1
+    2 0-1
+    3 1-0
+     */
+
+    public void gameOfLife(int[][] board) {
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1}; // 重点1 建立矢量坐标
+        int[] dc = {-1, 0, 1,  -1, 1, -1, 0, 1};
+        for (int i = 0; i < board.length; i++) {  // r
+            for (int j = 0; j < board[0].length; j++) { // c
+                int cnt = 0;
+                for (int k = 0; k < 8; k++) {
+                    int r = i + dr[k];
+                    int c = i + dc[k];        //  bug1
+                    if (r >= 0 && r < board.length
+                         && c >= 0 && c < board[0].length
+                         &&(board[r][c] == 1 ||board[r][c] == 3))  //活着的cnt
+                        cnt++;
+
+                }
+
+                if(board[i][j] == 1 && (cnt < 2 || cnt > 3)) board[i][j] = 3; // 1, 3 : 1=>0
+                else if (board[i][j] == 0 && cnt == 3) board[i][j] = 2; //2 4 : 0=>1
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 2) board[i][j] = 1;
+                if (board[i][j] == 3) board[i][j] = 0;
+            }
+        }
+
+    }
+
+}
+/** 题
+ *
+ * http:
+ *
+
+
+ */
+
+/** Solution
+ * 时间  空间
+ *
+ *
+ *
+ 参考网站
+
+ 思路 :
+
+ guan
+
+ 步骤1
+ 步骤2
+ 步骤3
+
+ bug1
+ bug2
+ bug3
+ */
