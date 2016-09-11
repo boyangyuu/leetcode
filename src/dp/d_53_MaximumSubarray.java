7494925e2a522ee25200d10f437fe876914c12f0
ybyGold <415098820@qq.com>
2016-09-11 17:30:58 +0800

    create acc 方法2 d_53_MaximumSubarray

--- src/dp/d_53_MaximumSubarray.java
+++ src/dp/d_53_MaximumSubarray.java
@@ -18,8 +18,19 @@ public class d_53_MaximumSubarray {
         return max;
     }
 
+    /*
+    可以感觉到 dp[]数组, 每次只用到了 i-1, i 两个数值, 并没有查询以往的数值 是不需要用数组的 改为temp即可
+     */
     public int maxSubArray1(int[] nums) {
-        return 0;
+        //int [] dp = new int[nums.length];
+        int temp = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            temp = temp > 0 ? temp + nums[i] : nums[i]; //bugs1 dp[i]存的是 尾数为 i情况下的最大值
+            max = temp > max ? temp : max;
+        }
+
+        return max;
     }
 }
 /** 题
