f6ae9f8cd52754937147a7bd3de0db66891da57f
ybyGold <415098820@qq.com>
2016-09-11 22:56:58 +0800

    acc  d_63_UniquePaths

--- src/dp/d_63_UniquePaths2.java
+++ src/dp/d_63_UniquePaths2.java
@@ -10,8 +10,8 @@ public class d_63_UniquePaths2 {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
                 else if (i==0 && j==0) dp[i][j] = obstacleGrid[i][j];  //和uniquePath 相比 其实只是unique 是提前设置好了 0 ?
                 else if (j==0) dp[i][j] = dp[i-1][j];  // ? 0 而已 , 而这道题是 需要从 0,0开始算
