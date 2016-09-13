434b76566e8944468143eca36d932ed8a37d5c7a
ybyGold <415098820@qq.com>
2016-09-13 23:02:29 +0800

    acc  d_91_DecodeWays

--- src/dp/d_91_DecodeWays.java
+++ src/dp/d_91_DecodeWays.java
@@ -8,27 +8,36 @@ public class d_91_DecodeWays {
     }
 
     public static int numDecodings(String s) {
-        if (s.length() <= 1) return 1;
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return s.charAt(0) == '0' ? 0 : 1;
         int[] dp = new int[s.length()];
-        dp[0] = 1;
-        dp[1] = help(s, 1) ? 2 : 1;
+        dp[0] = s.charAt(0) == '0' ? 0 : 1;
+        if(s.charAt(0) == '0')dp[1] = 0;
+        else if(s.charAt(0) - '0' > 2 && s.charAt(1) == '0') dp[1] = 0;
+        else if(s.charAt(0) - '0' == 1 && s.charAt(1) != '0' || s.charAt(0) - '0' == 2 && s.charAt(1) - '0' < 7 && s.charAt(1) != '0') dp[1] = 2;
+        else dp[1] = 1;
         for (int i = 2; i < s.length(); i++) {
-            if (help(s, i)) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            } else {
+            dp[i] = 0;                   //todo 数组初始化默认为0, 这行可以不写 只是为了可读性
+            if (s.charAt(i) != '0') {
                 dp[i] = dp[i - 1];
+            } else if (isAdd(s, i)) { //todo bug
+                dp[i] += dp[i - 2];
             }
         }
 
         return dp[s.length() - 1];
     }
 
-    public static boolean help(String s, int i){
-        if (i < 1) return false;
-        int c1 = Integer.parseInt(s.substring(i-1,i));   //TODO 重点 char 转 int
+
+    // check if can add dp(n-2)
+    public static boolean isAdd(String s, int i){
+        int c1 = s.charAt(i-1) - '0';   //TODO 重点 char 转 int 或者  '?' - '0'
         int c2 = Integer.parseInt(s.substring(i,i+1));   //TODO 教学 subtring 参数1 第一个字符的位置(from 1) 第二个字符的位置
-        //System.out.println("c1: " + c1 + " ,c2: " + c2);
-        if (c1 == 1 || c1 == 2 && c2 < 7) { // 10-26
+        int v = c1 * 10 + c2;
+        if (c1 == 0) {
+            return false;
+        }
+        if (10 <= v && v < 27) { // 10-26
             return true;
         } else
             return false;
@@ -49,15 +58,23 @@ public class d_91_DecodeWays {
  *
  *
  参考网站
+ https://discuss.leetcode.com/topic/35840/java-clean-dp-solution-with-explanation
+ https://discuss.leetcode.com/topic/54450/2ms-java-dp-solution
 
  思路 :
 如何迭代
 
+ 错误:
  f(n)
  10 <26 : f(n-1) + f(n-2)
  else   : f(n-1)
 
+ 正确:
+ f(n) = 0;
+ x[n] != 0 : f(n) +=f(n-1)
+ x[n-1] != 0 && ( x[n-1],x[n] 可以组合为1个字母 )   : f(n) += f(n-2)   1 2 [3] => 1, 2, [3] 和 1, [23]
 
+方法不够简洁!!
 
  步骤1
  步骤2
