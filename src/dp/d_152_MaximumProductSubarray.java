aada6b26c5bb37df339502f1e6012e48b0f89212
ybyGold <415098820@qq.com>
2016-09-17 16:22:15 +0800

    【bug 】d_152_MaximumProductSubarray

--- src/dp/d_152_MaximumProductSubarray.java
+++ src/dp/d_152_MaximumProductSubarray.java
@@ -10,9 +10,9 @@ public class d_152_MaximumProductSubarray {
     public int maxProduct(int[] nums) {
         int max = nums[0], min = nums[0], ret = max;
 
-        for (int i = 1; i < nums.length; i++) { // todo bug1
-            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
-            min = Math.min(Math.min(max * nums[i], min * nums[i]), nums[i]);
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]); // todo bug1
+            min = Math.min(Math.min(max * nums[i], min * nums[i]), nums[i]); // todo bug1
             ret = Math.max(ret, max);
 //            System.out.println("max "+max);
 //            System.out.println("min "+min);
