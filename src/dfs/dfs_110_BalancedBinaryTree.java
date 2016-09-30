a51f1da6aab88c380f5c5ad4f1819a478f00cb09
ybyGold <415098820@qq.com>
2016-09-30 19:47:40 +0800

    ct dfs_110_BalancedBinaryTree

---
+++ src/dfs/dfs_110_BalancedBinaryTree.java
@@ -0,0 +1,71 @@
+package dfs;
+
+import tree.TreeNode;
+
+public class dfs_110_BalancedBinaryTree {
+    public static void main(String[] args) {
+        //TreeNode
+        TreeNode node;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        int deep = getMaxDepth(root);
+        if (deep == -1) return false;
+        else return true;
+    }
+
+    public int getMaxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = getMaxDepth(root.left);
+        if (left == -1) return -1;
+        int right = getMaxDepth(root.right);
+        if (right == -1) return -1;
+
+        if (Math.abs((left - right)) > 1) return -1; //todo!! sov1
+        return Math.max(left, right) + 1;
+    }
+
+
+}
+/** 题
+ *
+ * http:
+ *
+
+
+ */
+
+/** Solution
+ * 时间  空间
+ *
+ *
+ *
+ 参考网站
+
+ 在计算deep的时候
+ 每个节点的 左右子树深度 相差不超过 1
+
+
+ TODO solotion
+
+
+ TODO case
+
+ TODO bug
+ bug1
+
+
+ bug2
+
+ bug3
+ */
+
+/*
+TODO tutorial
+
+
+TODO follow
+
+
+ */
